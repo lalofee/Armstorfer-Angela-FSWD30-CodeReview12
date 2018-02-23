9bb3d7698f34bbf74fc331d55b328f71af6321e3
lalofee <armstorfer@silverserver.at>
2018-02-23 15:20:01 +0100

    modified footer and sidebar

--- wp-content/themes/cr12_Armstorfer_Angela_traveler/header.php
+++ wp-content/themes/cr12_Armstorfer_Angela_traveler/header.php
@@ -1,43 +1,32 @@
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="<?php bloginfo('description'); ?>">
-    <title><?php bloginfo('name'); ?></title>
-    
-    <!-- Bootstrap core CSS -->
-    <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Custom styles for this template -->
-    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
-   <?php wp_head(); ?>
-    <!-- <link href="css/blog.css" rel="stylesheet"> -->
-  </head> 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
+</head>
 
-  
-  <body <?php body_class(); ?>>
+<body <?php body_class(); ?>>
 
-    <div class="blog-masthead">
-      <div class="container">
-        
-        <?php wp_nav_menu( array( 
-            'theme_location' => 'header-menu', 
-            'menu_class' => 'blog-nav list-inline' 
-) ); ?>
-      
-      </div>
+<div class="blog-masthead">
+    <div class="container">
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'blog-nav list-inline'  ) ); ?>
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="blog-header">
+        <h1 class="blog-title"><?php bloginfo( 'name' ); ?></h1>
+        <?php $description = get_bloginfo( 'description', 'display' ); ?>
+        <?php if($description) { ?><p class="lead blog-description"><?php echo $description ?></p><?php } ?>
     </div>
 
- <div class="container">
-
-
-  <div class="jumbotron text-center">
-          <h1>The Travel Agency</h1> 
-          <p>We provide the best tips for travelers around the world!</p> 
-        </div>
-
-
-
-      <div class="row">
+    <div class="row">
